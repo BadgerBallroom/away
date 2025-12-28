befbdb1486c77bb741119ea2b48c21ff2517ac23
David Tsai <david@wecsam.com>
2025-12-27 20:39:03 -0600

    Fix whitespace

--- src/model/EnumCollator.ts
+++ src/model/EnumCollator.ts
@@ -7,25 +7,25 @@ export default class EnumCollator<E> {
      * given order. If the array to be sorted contains a value that was not specified here, that value will be put at
      * the end of the array. If the array to be sorted contains an empty string, it will be sorted to the beginning of
      * the array. For example, given this enum:
-     * 
+     *
      *  ```
      *  enum Foo { Bar, Baz, Qux, Quux }
      *  ```
-     * 
+     *
      * If you construct an `EnumCollator` as follows:
-     * 
+     *
      *  ```
      *  const collator = new EnumCollator([Foo.Qux, Foo.Bar, Foo.Baz]);
      *  ```
-     * 
+     *
      * You could then sort an array of `Foo` values as follows:
-     * 
+     *
      *  ```
      *  const foos = [Foo.Quux, Foo.Baz, Foo.Qux];
      *  foos.sort(collator.compare);
      *  // Result: [Foo.Qux, Foo.Baz, Foo.Quux]
      *  ```
-     * 
+     *
      * @param enumOrder The order in which the enum values should be sorted
      */
     constructor(enumOrder: E[]) {
